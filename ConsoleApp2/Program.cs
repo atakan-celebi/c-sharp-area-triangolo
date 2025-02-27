f239c840363f6ffa6b02e9a12b532e054f4df136
Atakan Celebi <atakancelebi2007@gmail.com>
2025-02-27 18:11:39 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("inserisci la base e l'altezza del triangolo");
+float bas = float.Parse(Console.ReadLine());
+float altezza = float.Parse(Console.ReadLine());
+Console.WriteLine("l'area è grande: {0}",(bas*altezza)/2);
